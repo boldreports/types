20078ad6ede535892944c17a6a039c5b3a874314
vishnu <vishnu.tamilventhan@syncfusion.com>
2023-10-30 15:50:11 +0530

    feature(REPORT-333362): version change

--- reports.all/index.d.ts
+++ reports.all/index.d.ts
@@ -1,6 +1,6 @@
 /**
  *  filename: index.d.ts
- *  version : 1.0.5
+ *  version : 5.3.8
  *  Copyright Syncfusion Inc. 2001 - 2018. All rights reserved.
  *  Use of this code is subject to the terms of our license.
  *  A copy of the current license can be obtained at any time by e-mailing
